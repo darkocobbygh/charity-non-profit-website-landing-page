665f79e807e5fb057eb11d056ba7823141e74331
darkocobbygh <darkogaucho@gmail.com>
2023-09-30 23:50:05 +0000

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
+import logo from '../assets/VGF Logo File  (1) 1.png'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-start pt-3'>
-        <Link>Logo</Link>
+        <Link>
+          <img src={logo} alt="" />
+        </Link>
       <ul className='h-16 w-96 flex gap-y-10'>
         <li>
             <Link>Home</Link>
